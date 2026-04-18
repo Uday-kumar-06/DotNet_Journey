57d8dff27f500a1ba7c3548e7fb8bfe3e9c1c031
Uday-kumar-06 <udaykumarkella67@gmail.com>
2026-04-18 11:24:40 +0530

    Added collection code..

--- C_Sharp/C_Sharp_Collections/C_Sharp_Collections/Program.cs
+++ C_Sharp/C_Sharp_Collections/C_Sharp_Collections/Program.cs
@@ -1,1 +1,19 @@
-﻿Console.WriteLine("Hello, World!");
+﻿class Program{
+    public static void Main(string[] args)
+    {
+        //List
+        List<string> names = new List<string>();
+
+        for(int i = 0; i < 5; i++)
+        {
+            Console.WriteLine("Enter a name: ");
+            string name = Console.ReadLine();
+            names.Add(name);
+        }
+
+        foreach(string name in names)
+        {
+            Console.WriteLine(name);
+        }
+    }
+}
