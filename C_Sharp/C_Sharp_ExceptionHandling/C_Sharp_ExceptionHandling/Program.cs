f8cead7954def27a8288f5383d76bd4c93a563c4
Uday-kumar-06 <udaykumarkella67@gmail.com>
2026-04-20 17:43:23 +0530

    Made Some Changes

--- C_Sharp/C_Sharp_ExceptionHandling/C_Sharp_ExceptionHandling/Program.cs
+++ C_Sharp/C_Sharp_ExceptionHandling/C_Sharp_ExceptionHandling/Program.cs
@@ -1,1 +1,36 @@
-﻿Console.WriteLine("Hello, World!");
+﻿class MyException : Exception
+{
+    public MyException (string message): base(message)
+    {
+    }
+}
+class Program
+{
+    public static void Main()
+    {
+        int num = 1;
+        try
+        {
+            if (num > 10)
+            {
+                throw new MyException("This is a custom exception.");
+            }
+
+            int num2 = 0;
+            int result = num / num2;
+        }
+        catch (MyException ex)
+        {
+            Console.WriteLine($"Caught an exception: {ex.Message}");
+        }
+        catch (DivideByZeroException ex)
+        {
+            Console.WriteLine($"Caught a divide by zero exception: {ex.Message}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Caught a general exception: {ex.Message}");
+        }
+
+    }
+}
