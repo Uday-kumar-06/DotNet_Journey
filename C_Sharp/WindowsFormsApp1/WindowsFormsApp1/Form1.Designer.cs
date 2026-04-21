1ded2c1b9c2de458b491e05f767f65814e445c88
Uday-kumar-06 <udaykumarkella67@gmail.com>
2026-04-21 13:31:11 +0530

    stop watch

---
+++ C_Sharp/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(331, 330);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(169, 70);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(802, 321);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(169, 70);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(587, 422);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(169, 70);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "save";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(397, 117);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(130, 84);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(596, 117);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(130, 84);
+            this.richTextBox2.TabIndex = 4;
+            this.richTextBox2.Text = "";
+            this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(802, 117);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(130, 84);
+            this.richTextBox3.TabIndex = 5;
+            this.richTextBox3.Text = "";
+            this.richTextBox3.TextChanged += new System.EventHandler(this.richTextBox3_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1363, 504);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+    }
+}
+
---
+++ C_Sharp/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            // initialize timer for stopwatch
+            timer = new Timer();
+            timer.Interval = 1000; // 1 second
+            timer.Tick += Timer_Tick;
+
+            // initial UI state: show separate components
+            richTextBox1.Text = $"{hour:D2}"; // hours
+            richTextBox2.Text = $"{min:D2}";  // minutes
+            richTextBox3.Text = $"{sec:D2}";  // seconds
+            button2.Enabled = false;
+        }
+
+        int hour = 0;
+        int min = 0;
+        int sec = 0;
+        private Timer timer;
+        private bool running = false;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!running)
+            {
+                timer.Start();
+                running = true;
+                this.Text = "Running";
+                button1.Enabled = false;
+                button2.Enabled = true;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (running)
+            {
+                timer.Stop();
+                running = false;
+                this.Text = "Stopped";
+                button1.Enabled = true;
+                button2.Enabled = false;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // save current time (hh:mm:ss) to laps.txt
+            var current = $"{hour:D2}:{min:D2}:{sec:D2}";
+            var path = Path.Combine(Application.StartupPath, "laps.txt");
+            try
+            {
+                File.AppendAllText(path, current + Environment.NewLine);
+                MessageBox.Show($"Saved: {current}", "Saved", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Failed to save: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            sec++;
+            if (sec >= 60)
+            {
+                sec = 0;
+                min++;
+            }
+            if (min >= 60)
+            {
+                min = 0;
+                hour++;
+            }
+
+            // update separate displays
+            richTextBox1.Text = $"{hour:D2}";
+            richTextBox2.Text = $"{min:D2}";
+            richTextBox3.Text = $"{sec:D2}";
+        }
+
+        // empty handlers for designer wired events
+        private void richTextBox1_TextChanged(object sender, EventArgs e) { }
+        private void richTextBox2_TextChanged(object sender, EventArgs e) { }
+        private void richTextBox3_TextChanged(object sender, EventArgs e) { }
+    }
+}
