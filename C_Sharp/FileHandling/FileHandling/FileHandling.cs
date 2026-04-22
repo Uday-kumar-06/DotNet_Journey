e6ebed53af750e64b286ace0efd99715fbbe4a14
Uday-kumar-06 <udaykumarkella67@gmail.com>
2026-04-22 16:53:01 +0530

    File handling comcept

---
+++ C_Sharp/FileHandling/FileHandling/FileHandling.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace FileHandling
+{
+    internal class FileHandle
+    {
+
+        public void FileHandlingConcept()
+        {
+            
+
+            try
+            {
+                File.Create("test.txt").Close();
+                File.WriteAllText("test.txt", "Hello World what are doing ??");
+                File.AppendAllText("test.txt", "I am learning C#");
+                string content = File.ReadAllText("test.txt");
+                Console.WriteLine(content);
+
+                File.Copy("test.txt", "test_copy.txt");
+
+                using(StreamReader reader = new StreamReader("test_copy.txt"))
+                {
+                    string line;
+                    while((line = reader.ReadLine())!= null)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+
+                //File.Delete("test.txt");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+        }
+    }
+}
---
+++ C_Sharp/FileHandling/FileHandling/Program.cs
@@ -0,0 +1,10 @@
+﻿using FileHandling;
+
+class Program
+{
+    public static void Main()
+    {
+        FileHandling.FileHandle fileHandling = new FileHandling.FileHandle();
+        fileHandling.FileHandlingConcept();
+    }
+}
