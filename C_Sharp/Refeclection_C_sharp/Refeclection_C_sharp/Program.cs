08e699911383919c706463e3cff7f3677278b47f
Uday-kumar-06 <udaykumarkella67@gmail.com>
2026-04-23 19:46:53 +0530

    Refelection concept

---
+++ C_Sharp/Refeclection_C_sharp/Refeclection_C_sharp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Reflection;
+class Program
+{
+
+    public void Change(ref int a, out int b)
+    {
+        a = 1100;
+        b = 1200;
+    }
+    public static void Main(string[] args)
+    {
+        Type t = typeof(Refeclection_C_sharp.Students);
+
+        Console.WriteLine("Class Name: " + t.Name);
+        Console.WriteLine("Name Space : "+ t.Namespace);
+
+        MethodInfo[] methods = t.GetMethods();
+        Console.WriteLine("Methods:");
+
+        foreach(MethodInfo method2 in methods)
+        {
+            Console.WriteLine(method2.Name);
+        }
+
+        MethodInfo[] methods1 = t.GetMethods(
+        BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly);
+
+        foreach (MethodInfo method1 in methods1) { 
+            Console.WriteLine("Methods1 :"+method1.Name);
+        }
+
+        object obj = Activator.CreateInstance(typeof(Refeclection_C_sharp.Students),new object[] { "Uday", "A", 101 });
+
+         MethodInfo method = t.GetMethod("Display");
+         method.Invoke(obj, null);
+
+        int a = 20; int b;
+        Program p = new Program();
+        p.Change(ref a, out b);
+        Console.WriteLine("a: " + a + ", b: " + b);
+    }
+}
---
+++ C_Sharp/Refeclection_C_sharp/Refeclection_C_sharp/Students.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Refeclection_C_sharp
+{
+    internal class Students
+    {
+        public string name { get; set; }
+        public string section { get; set; }
+        public int rollno { get; set; } 
+
+
+
+        public Students(string name, string section, int rollno)
+        {
+            this.name = name;
+            this.section = section;
+            this.rollno = rollno;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"Name: {name}, Section: {section}, Roll No: {rollno}");
+        }
+    }
+}
