144220d5e631422a5a132f8b21359543435073e4
Uday-kumar-06 <udaykumarkella67@gmail.com>
2026-04-28 00:53:51 +0530

    Added some code

--- DotNet_practice/dotnet_practice/GameStore/GameStore.Api/Dtos/CreateGameDto.cs
+++ DotNet_practice/dotnet_practice/GameStore/GameStore.Api/Dtos/CreateGameDto.cs
@@ -1,6 +1,6 @@
 namespace GameStore.Api.Dtos;
-public record GameDto(
-    int Id,
+
+public record CreateGameDto(
     string Name,
     string Genre,
     decimal Price,
--- DotNet_practice/dotnet_practice/GameStore/GameStore.Api/Program.cs
+++ DotNet_practice/dotnet_practice/GameStore/GameStore.Api/Program.cs
@@ -1,16 +1,38 @@
+using GameStore.Api.Dtos;
+
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+const string GetGameEndpointName = "GetGame";
 
 List<GameDto> games = [
     new GameDto(1,"spider"," Advanture",19.99M, new DateOnly(1992, 7, 15)),
-    new GameDto(1,"ninja"," climb",10.99M, new DateOnly(1928, 3, 25)),
-    new GameDto(1,"Temple"," running",19.89M, new DateOnly(1992, 10, 1)),
-    new GameDto(1,"subway"," running",15.99M, new DateOnly(1954, 7, 5))
+    new GameDto(2,"ninja"," climb",10.99M, new DateOnly(1928, 3, 25)),
+    new GameDto(3,"Temple"," running",19.89M, new DateOnly(1992, 10, 1)),
+    new GameDto(4,"subway"," running",15.99M, new DateOnly(1954, 7, 5))
 ];
-// GET 
+// GET /games
 app.MapGet("/games", () => games);
-app.MapGet("/student", () => "Hi");
 
+//GET /games/1
+app.MapGet("/games/{id}", (int id) =>games.Find(game => game.Id == id))
+    .WithName(GetGameEndpointName);
+
+// POST /games
+app.MapPost("/games", (CreateGameDto newGame) =>
+{
+    GameDto gameDto = new(
+        games.Count + 1,
+        newGame.Name,
+        newGame.Genre,
+        newGame.Price,
+        newGame.ReleaseDate
+    );
+
+    games.Add(gameDto);
+
+    return Results.CreatedAtRoute(GetGameEndpointName, new {id = gameDto.Id}, gameDto);
+});
 
 app.Run();
