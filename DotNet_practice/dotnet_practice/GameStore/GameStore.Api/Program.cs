4e3bf6bc4a041d57681273843d63bade3b179652
Uday-kumar-06 <udaykumarkella67@gmail.com>
2026-04-27 23:05:52 +0530

    Added the code

--- DotNet_practice/dotnet_practice/GameStore/GameStore.Api/Program.cs
+++ DotNet_practice/dotnet_practice/GameStore/GameStore.Api/Program.cs
@@ -1,8 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+
+List<GameDto> games = [
+    new GameDto(1,"spider"," Advanture",19.99M, new DateOnly(1992, 7, 15)),
+    new GameDto(1,"ninja"," climb",10.99M, new DateOnly(1928, 3, 25)),
+    new GameDto(1,"Temple"," running",19.89M, new DateOnly(1992, 10, 1)),
+    new GameDto(1,"subway"," running",15.99M, new DateOnly(1954, 7, 5))
+];
 // GET 
-app.MapGet("/games", () => "Hello World!");
+app.MapGet("/games", () => games);
 app.MapGet("/student", () => "Hi");
 
 
