deba2352a3cbf963e5d724f9f2fb0d26de2a9b64
Uday-kumar-06 <udaykumarkella67@gmail.com>
2026-04-29 00:12:15 +0530

    Added the code

---
+++ C_Sharp/DesginPattern_Assignment_day10/DesginPattern_Assignment_day10/Factory/Interface1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesginPattern_Assignment_day10.Factory
+{
+    internal interface Interface1
+    {
+    }
+}
---
+++ C_Sharp/DesginPattern_Assignment_day10/DesginPattern_Assignment_day10/Singleton/Logger.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesginPattern_Assignment_day10
+{
+    sealed class Logger
+    {
+        private readonly Object _lock = new Object();
+        private Logger logger;
+
+        private Logger()
+        {
+            Console.WriteLine("Logger instance created.....");
+        }
+        public  Logger GetInstance()
+        {
+            if (logger == null)
+            {
+                lock (_lock)
+                {
+                    logger = new Logger();
+                }
+            }
+            return logger;
+        }
+        
+    }
+}
