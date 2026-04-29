ca557eb8bd2106e59323d46dc941e56e550ab6df
Uday-kumar-06 <udaykumarkella67@gmail.com>
2026-04-29 19:26:59 +0530

    Added the code

---
+++ C_Sharp/User_Authentication_Assignment/User_Authentication_Assignment/UserAuthentication/model/User.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace User_Authentication_Assignment.model
+{
+    internal class User
+    {
+    }
+}
---
+++ C_Sharp/User_Authentication_Assignment/User_Authentication_Assignment/UserAuthentication/model/UserCreation.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace User_Authentication_Assignment.UserAuthentication.model
+{
+    internal class UserCreation
+    {
+    }
+}
