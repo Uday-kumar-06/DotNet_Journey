425f45b2621b2ad4dc8994fa31820a310140436d
Uday-kumar-06 <udaykumarkella67@gmail.com>
2026-05-02 16:15:05 +0530

    Made some Changes and created the Assignment

---
+++ C_Sharp/CalculatorSolution/TestProject1/UnitTest1.cs
@@ -0,0 +1,47 @@
+﻿    using NUnit.Framework;
+using CalculatorLib;
+using System;
+
+namespace CalculatorTests
+{
+    public class Tests
+    {
+        private Calculator calculator;
+
+        [SetUp]
+        public void Setup()
+        {
+            calculator = new Calculator();
+        }
+
+        [Test]
+        public void Add_ReturnsCorrectResult()
+        {
+            Assert.That(calculator.Add(5, 3), Is.EqualTo(8));
+        }
+
+        [Test]
+        public void Subtract_ReturnsCorrectResult()
+        {
+            Assert.That(calculator.Subtract(10, 4), Is.EqualTo(6));
+        }
+
+        [Test]
+        public void Multiply_ReturnsCorrectResult()
+        {
+            Assert.That(calculator.Multiply(3, 4), Is.EqualTo(12));
+        }
+
+        [Test]
+        public void Divide_ReturnsCorrectResult()
+        {
+            Assert.That(calculator.Divide(10, 2), Is.EqualTo(5));
+        }
+
+        [Test]
+        public void Divide_ByZero_ThrowsException()
+        {
+            Assert.Throws<DivideByZeroException>(() => calculator.Divide(10, 0));
+        }
+    }
+}
