db12fd5ab524bd9c7206102072727c3166c35f8e
Uday-kumar-06 <udaykumarkella67@gmail.com>
2026-05-04 02:43:24 +0530

    Added some code to application

--- DotNet_practice/dotnet_practice/GameStore/GameStore.Api/Dtos/CreateGameDto.cs
+++ DotNet_practice/dotnet_practice/GameStore/GameStore.Api/Dtos/CreateGameDto.cs
@@ -3,9 +3,11 @@ using System.ComponentModel.DataAnnotations;
 namespace GameStore.Api.Dtos;
 
 public record CreateGameDto(
-    [Required]
+    [Required][StringLength(50)]
     string Name,
+    [Required][StringLength(50)]
     string Genre,
+    [Range(1,2000)]
     decimal Price,
     DateOnly ReleaseDate
 );
--- DotNet_practice/dotnet_practice/GameStore/GameStore.Api/Dtos/UpdateGameDto.cs
+++ DotNet_practice/dotnet_practice/GameStore/GameStore.Api/Dtos/UpdateGameDto.cs
@@ -1,6 +1,10 @@
+using System.ComponentModel.DataAnnotations;
 public record UpdateGameDto(
+    [Required][StringLength(50)]
     string Name,
+    [Required][StringLength(50)]
     string Genre,
+    [Range(1,2000)]
     decimal Price,
     DateOnly ReleaseDate
 );
