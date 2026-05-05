ba4718bda962e9eaac9cb3a58268f9d270eb0aea
Uday-kumar-06 <udaykumarkella67@gmail.com>
2026-05-05 15:38:17 +0530

    Added the code

--- DotNet_practice/dotnet_practice/GameStore/GameStore.Api/Program.cs
+++ DotNet_practice/dotnet_practice/GameStore/GameStore.Api/Program.cs
@@ -1,9 +1,11 @@
-using GameStore.Api.Dtos;
-
+using GameStore.Api.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddValidation();
+//Dependency Injection
+var connString = "Data Source=GameStore.db";
+builder.Services.AddSqlite<GameStoreContext>(connString);
 var app = builder.Build();
 
 app.MapGamesEndpoints();
