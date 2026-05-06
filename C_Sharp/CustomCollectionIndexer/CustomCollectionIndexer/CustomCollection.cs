f5b25245e4fb7b4386625ae8088566d6ed327305
Uday-kumar-06 <udaykumarkella67@gmail.com>
2026-05-06 23:31:20 +0530

    Added the commit

---
+++ C_Sharp/CustomCollectionIndexer/CustomCollectionIndexer/CustomCollection.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomCollectionIndexer
+{
+    public class CustomCollection
+    {
+        List<string> list = new List<string>();
+
+        public string this[int index]
+        {
+            get
+            {
+                if(index <0 || index >= list.Count)
+                {
+                    throw new IndexOutOfRangeException("Index is out of range.");
+                }
+                return list[index];
+            }
+            set
+            {
+                if(index < 0)
+                {
+                    throw new NegitiveException("Index cannot be negative.");
+                }
+                if (index < list.Count)
+                {
+                    list[index] = value;
+                }
+                else
+                {
+                    while (list.Count <= index)
+                    {
+                        list.Add(null);
+                    }
+                    list[index] = value;
+                }
+            }
+        }
+    }
+
+    class NegitiveException : Exception
+    {
+        public NegitiveException(string message): base(message) { }
+    }
+}
---
+++ C_Sharp/CustomCollectionIndexer/CustomCollectionIndexer/Program.cs
@@ -0,0 +1,29 @@
+﻿using CustomCollectionIndexer;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        CustomCollection collection = new CustomCollection();
+        try
+        {
+            
+            for (int i = 0; i < 5; i++)
+            {
+                collection[i] = $"Item {i}";
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(collection[i]);
+            }
+        }
+        catch (IndexOutOfRangeException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }catch(Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+    }
+}
