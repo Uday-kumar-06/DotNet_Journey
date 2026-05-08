818fc717fa61a85d998867004f333cb795cbe482
Uday-kumar-06 <udaykumarkella67@gmail.com>
2026-05-08 15:54:27 +0530

    Added the practice code for the dotnet

---
+++ DotNet_MVC_Practice/FirstMVCWebApp/FirstMVCWebApp/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using FirstMVCWebApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FirstMVCWebApp.Data
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+    {
+        //In place of this we are using Primary Constructor
+        //
+        //public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        //{
+        //}
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ DotNet_MVC_Practice/FirstMVCWebApp/FirstMVCWebApp/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FirstMVCWebApp.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        public string UserName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
+    }
+}
