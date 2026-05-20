cdc284dc454cb067fe53c0b26acb761ea1002ba9
Uday-kumar-06 <udaykumarkella67@gmail.com>
2026-05-20 19:10:40 +0530

    Added the code

---
+++ DotNet_MVC_Practice/Practice_Assignment_Day16/MvcModelBindingDemo/MvcModelBindingDemo/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcModelBindingDemo.Models;
+
+namespace MvcModelBindingDemo.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Register(User user)
+        {
+            return View("Result", user);
+        }
+    }
+}
