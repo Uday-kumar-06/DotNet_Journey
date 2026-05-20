6d3c0285dce8a2c9c9f0198595b7ca5ac42b02bf
Uday-kumar-06 <udaykumarkella67@gmail.com>
2026-05-20 23:26:52 +0530

    Started with miniProject

---
+++ DotNet_MVC_Practice/SecureECommercePortal/SecureECommercePortal/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SecureECommercePortal.Models;
+
+namespace SecureECommercePortal.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
