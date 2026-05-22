d676e27652af1af206ea61bbc44f01e30770f864
Uday-kumar-06 <udaykumarkella67@gmail.com>
2026-05-22 23:10:07 +0530

    Added the code

--- DotNet_MVC_Practice/Practice_Assignment_One_Day17/RegistrationValidationSystem/RegistrationValidationSystem/Models/UserRegistration.cs
+++ DotNet_MVC_Practice/Practice_Assignment_One_Day17/RegistrationValidationSystem/RegistrationValidationSystem/Models/UserRegistration.cs
@@ -1,6 +1,25 @@
-﻿namespace RegistrationValidationSystem.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistrationValidationSystem.Models
 {
     public class UserRegistration
     {
+        [Required(ErrorMessage = "Username is required")]
+        [StringLength(20, MinimumLength = 3,
+            ErrorMessage = "Username must be between 3 and 20 characters")]
+        public string Username { get; set; }
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email format")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(15, MinimumLength = 6,
+            ErrorMessage = "Password must be between 6 and 15 characters")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "Confirm Password is required")]
+        [DataType(DataType.Password)]
+        [Compare("Password",
+            ErrorMessage = "Password and Confirm Password must match")]
+        public string ConfirmPassword { get; set; }
     }
 }
--- DotNet_MVC_Practice/Practice_Assignment_One_Day17/RegistrationValidationSystem/RegistrationValidationSystem/Pages/Register.cshtml.cs
+++ DotNet_MVC_Practice/Practice_Assignment_One_Day17/RegistrationValidationSystem/RegistrationValidationSystem/Pages/Register.cshtml.cs
@@ -1,12 +1,30 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RegistrationValidationSystem.Models;
 
 namespace RegistrationValidationSystem.Pages
 {
     public class RegisterModel : PageModel
     {
+        [BindProperty]
+        public UserRegistration Registration { get; set; }
+
+        public string SuccessMessage { get; set; }
+
         public void OnGet()
         {
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            SuccessMessage = "Registration Successful!";
+
+            return Page();
         }
     }
 }
