0a6eac78460f135809efeb1593aacf60e48ff5fd
Uday-kumar-06 <udaykumarkella67@gmail.com>
2026-05-24 15:45:12 +0530

    Added the code

---
+++ DotNet_MVC_Practice/Practice_Assignment_One_Day18/ECommerceRoutingApp/ECommerceRoutingApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ECommerceRoutingApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
