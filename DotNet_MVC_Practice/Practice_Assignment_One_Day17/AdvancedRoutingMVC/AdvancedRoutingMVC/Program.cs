3b8261cfa1bee00da9f439b1b3d4781d2fd6b3c5
Uday-kumar-06 <udaykumarkella67@gmail.com>
2026-05-24 15:30:31 +0530

    Added the code

--- DotNet_MVC_Practice/Practice_Assignment_One_Day17/AdvancedRoutingMVC/AdvancedRoutingMVC/Program.cs
+++ DotNet_MVC_Practice/Practice_Assignment_One_Day17/AdvancedRoutingMVC/AdvancedRoutingMVC/Program.cs
@@ -4,7 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 
-// Register custom constraint
+
 builder.Services.Configure<RouteOptions>(options =>
 {
     options.ConstraintMap.Add("validguid", typeof(GuidConstraint));
@@ -19,26 +19,16 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-
-// ================= COMPLEX ROUTES =================
-
-// Route: /Products/Electronics/101
 app.MapControllerRoute(
     name: "products",
     pattern: "Products/{category}/{id:int}",
     defaults: new { controller = "Products", action = "Details" });
 
-
-// Route: /Users/john/Orders
 app.MapControllerRoute(
     name: "userorders",
     pattern: "Users/{username}/Orders",
     defaults: new { controller = "Users", action = "Orders" });
 
-
-// ================= CUSTOM CONSTRAINT ROUTE =================
-
-// Route with GUID validation
 app.MapControllerRoute(
     name: "guidroute",
     pattern: "Dashboard/{id:validguid}",
