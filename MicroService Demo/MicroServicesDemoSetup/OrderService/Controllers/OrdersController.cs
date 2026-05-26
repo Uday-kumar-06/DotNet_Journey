7e1c53cc8bfa4aa8293d23dc744afe7e29f8abc7
Uday-kumar-06 <udaykumarkella67@gmail.com>
2026-05-26 10:49:57 +0530

    Added some code

---
+++ MicroService Demo/MicroServicesDemoSetup/OrderService/Controllers/OrdersController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OrderService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetOrders()
+        {
+            var orders = new List<object>
+            {
+                new { Id = 1, Product = "Product A", Quantity = 2 },
+                new { Id = 2, Product = "Product B", Quantity = 1 }
+            };
+            return Ok(orders);
+        }
+    }
+}
---
+++ MicroService Demo/MicroServicesDemoSetup/ProductService/Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProductService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            var products = new List<object>
+            {
+                new { Id = 1, Name = "Product A", Price = 10.0 },
+                new { Id = 2, Name = "Product B", Price = 20.0 }
+            };
+            return Ok(products);
+        }
+    }
+}
