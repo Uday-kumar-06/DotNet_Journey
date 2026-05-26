28c919764823a41df9260609b035b6048bf2025b
Uday-kumar-06 <udaykumarkella67@gmail.com>
2026-05-26 10:50:36 +0530

    Added some code

---
+++ MicroService Demo/ECommerceMicroservicesDemo1/ECommerceMicroservices/ProductService/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProductService.Controllers
+{
+    public class ProductController : Controller
+    {
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            var products = new List<string>
+            {
+                "Laptop",
+                "Mobile",
+                "Keyboard"
+            };
+
+            return Ok(products);
+        }
+    }
+}
--- MicroService Demo/ECommerceMicroservicesDemo1/ECommerceMicroservices/ProductService/Program.cs
+++ MicroService Demo/ECommerceMicroservicesDemo1/ECommerceMicroservices/ProductService/Program.cs
@@ -1,17 +1,33 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+// Add services to container
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+// Swagger services
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Microservice API",
+        Version = "v1"
+    });
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure pipeline
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "Microservice API V1");
+    });
 }
 
 app.UseHttpsRedirection();
