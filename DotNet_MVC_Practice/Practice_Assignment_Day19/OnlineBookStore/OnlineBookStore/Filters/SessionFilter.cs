cd39939872b4c1ed477d2bd8cd156c07593ef1e0
Uday-kumar-06 <udaykumarkella67@gmail.com>
2026-05-27 17:51:03 +0530

    Added a assignement

--- DotNet_MVC_Practice/Practice_Assignment_Day19/OnlineBookStore/OnlineBookStore/Filters/SessionFilter.cs
+++ DotNet_MVC_Practice/Practice_Assignment_Day19/OnlineBookStore/OnlineBookStore/Filters/SessionFilter.cs
@@ -1,6 +1,25 @@
-﻿namespace OnlineBookStore.Filters
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace OnlineBookStore.Filters
 {
-    public class SessionFilter
+    public class SessionFilter : ActionFilterAttribute
     {
+        public override void OnActionExecuting(
+            ActionExecutingContext context)
+        {
+            var sessionValue =
+                context.HttpContext.Session.GetString("Cart");
+
+            if (string.IsNullOrEmpty(sessionValue))
+            {
+                context.Result = new RedirectToActionResult(
+                    "Index",
+                    "Books",
+                    null);
+            }
+
+            base.OnActionExecuting(context);
+        }
     }
 }
