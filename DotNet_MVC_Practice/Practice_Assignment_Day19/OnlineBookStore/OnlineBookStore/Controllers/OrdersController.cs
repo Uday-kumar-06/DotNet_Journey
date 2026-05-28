036319c3032433791321fb7d1b91938cb48942d9
Uday-kumar-06 <udaykumarkella67@gmail.com>
2026-05-28 13:37:08 +0530

    Added

--- DotNet_MVC_Practice/Practice_Assignment_Day19/OnlineBookStore/OnlineBookStore/Controllers/OrdersController.cs
+++ DotNet_MVC_Practice/Practice_Assignment_Day19/OnlineBookStore/OnlineBookStore/Controllers/OrdersController.cs
@@ -1,14 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using OnlineBookStore.Filters;
 
 namespace OnlineBookStore.Controllers
 {
     public class OrdersController : Controller
     {
+        [SessionFilter]
         public IActionResult Summary()
         {
             return View();
         }
 
+        [SessionFilter]
         public IActionResult Confirmation()
         {
             return View();
