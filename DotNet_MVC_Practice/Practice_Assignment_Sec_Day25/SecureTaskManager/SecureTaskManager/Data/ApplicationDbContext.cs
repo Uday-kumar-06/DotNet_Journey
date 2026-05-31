bc3aae7036635c4d9a7c057139b22c97e6d2c389
Uday-kumar-06 <udaykumarkella67@gmail.com>
2026-05-31 15:41:09 +0530

    Added another project

---
+++ DotNet_MVC_Practice/Practice_Assignment_Sec_Day25/SecureTaskManager/SecureTaskManager/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SecureTaskManager.Models;
+
+namespace SecureTaskManager.Data
+{
+    public class ApplicationDbContext
+        : IdentityDbContext
+    {
+        public ApplicationDbContext(
+            DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TaskItem> Tasks { get; set; }
+    }
+}
