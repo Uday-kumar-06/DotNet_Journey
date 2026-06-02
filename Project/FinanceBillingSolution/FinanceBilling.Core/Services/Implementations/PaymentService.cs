ed281eadc42945c8f8f1516fb7374f382ad6966d
Uday-kumar-06 <udaykumarkella67@gmail.com>
2026-06-02 12:03:19 +0530

    Struct of Project

---
+++ Project/FinanceBillingSolution/FinanceBilling.Core/Services/Implementations/PaymentService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinanceBilling.Core.Services.Implementations
+{
+    internal class PaymentService
+    {
+    }
+}
